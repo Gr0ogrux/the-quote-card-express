60667b6f280a203f58eea7062a94cde3423aefac
Gr0ogrux <fields.andy@gmail.com>
2026-02-24 18:48:17 -0500

    Added nodemon, quotes, and loopThroughQuotes function

---
+++ public/js/index.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "I am Groot.",
+        author: "Groot",
+    },
+
+    {
+        quote: "You miss 100% of the shots you don't take.",
+        author: "Wayne Gretzky",
+    },
+
+    {
+        quote: "You miss 100% of the shots you don't take. - Wayne Gretzky",
+        author: "Michael Scott",
+    },
+
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
