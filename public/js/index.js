90c0ce7ee2401dcfc285fa9a227d107876752937
Gr0ogrux <fields.andy@gmail.com>
2026-02-28 22:20:40 -0500

    installed dotenv and cors.  Updated getRandomImage function

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,20 @@ async function getRandomImage() {
 
 getRandomImage();
 
+async function getRandomImage() {
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.data;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 /* const quotes = [
     {
         quote: "All hands! Abandon ship!",
